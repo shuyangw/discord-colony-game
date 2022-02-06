eeeb6cb024789924304a21f23abf6775b513ef57
Shuyang <qwqw1337@gmail.com>
2022-02-05 19:47:10 -0500

    Revive project and redo initial commits

--- CitybuilderBot/Program.cs
+++ CitybuilderBot/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 
-namespace FriendshipDrive
+namespace CitybuilderBot
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
